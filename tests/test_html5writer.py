d90cdcbe404e3b63c752a09a9c101f26ce2f5d50
André Felipe Dias <andref.dias@pronus.eng.br>
2012-07-27 18:18:23 -0300

    tests: test head

--- tests/test_html5writer.py
+++ tests/test_html5writer.py
@@ -14,11 +14,12 @@ from tempfile import gettempdir
 
 tmpdir = gettempdir()
 
-def rst_to_html5_body(rst, indent_output=False, show_id=False):
+def rst_to_html5(rst, indent_output=False, show_id=False):
     overrides = {'indent_output': indent_output, 'show_id': show_id}
     parts = publish_parts(writer=HTML5Writer(), source=rst,
                           settings_overrides=overrides)
-    return parts['body']
+    return parts
+
 
 def extract_variables(module):
     '''
@@ -29,25 +30,37 @@ def extract_variables(module):
     return ((v, getattr(module, v)) for v in dir(module)
         if not v.startswith('__') and isinstance(getattr(module, v), dict))
 
+
+def test_head():
+    '''
+    test the head part of a rst2html5 conversion
+    '''
+    import test_data_head
+    for test_name, case in extract_variables(test_data_head):
+        out = case.pop('out')
+        yield _test_part, 'head', test_name, case, out
+
+
 def test_body():
     '''
     test the body part of a rst2html5 conversion
     '''
     import test_data_body
-    for name, case in extract_variables(test_data_body):
+    for test_name, case in extract_variables(test_data_body):
         out = case.pop('out')
-        yield _test_body, name, case, out
+        yield _test_part, 'body', test_name, case, out
 
-def _test_body(name, params, out):
+
+def _test_part(part_name, test_name, params, out):
     try:
-        assert_equals(rst_to_html5_body(**params), out)
+        assert_equals(rst_to_html5(**params)[part_name], out)
     except AssertionError as error:
         '''
         write temp files to help manual testing
         '''
-        filename = os.path.join(tmpdir, name)
+        filename = os.path.join(tmpdir, test_name)
         with codecs.open(filename + '.rst', encoding='utf-8', mode='w') as f:
             f.write(params['rst'])
 
-        error.args = ('%s: %s' % (name, error.message), )
+        error.args = ('%s: %s' % (test_name, error.message), )
         raise error
