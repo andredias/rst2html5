eea5f1dedf7c22086f2e7a76a6cabe0ba2988f7f
André Felipe Dias <andref.dias@pronus.eng.br>
2013-06-17 20:14:30 -0300

    adjust setup.py to 1.0

--- setup.py
+++ setup.py
@@ -13,19 +13,18 @@ with open('README.txt') as f:
 
 setup(
     name='rst2html5',
-    version='0.10',
+    version='1.0',
     author='André Felipe Dias',
-    author_email='andref.dias@gmailself.com',
+    author_email='andref.dias@gmail.com',
     url='https://bitbucket.org/andre_felipe_dias/rst2html5',
     keywords=["restructuredtext", "rst", "html5", "doctutils"],
-    description='Generates (X)HTML5 documents from standalone '
-                'reStructuredText sources',
+    description='Generates (X)HTML5 documents from standalone reStructuredText sources',
     long_description=long_description,
     license='MIT License',
     platforms='any',
     install_requires=['docutils', 'Genshi >= 0.6', 'Pygments'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
