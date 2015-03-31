29db6ac8e2c5811889df602d445eb4ecb963d86e
André Felipe Dias <andre.dias@pronus.io>
2015-03-31 08:01:48 -0300

    fix #22 | setup.py produces broken distributions
    --HG--
    branch : sphinx

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     zip_safe=False,
     packages=find_packages("src"),
     package_dir={"": "src"},
-    py_modules=[splitext(basename(i))[0] for i in glob.glob("src/*.py")],
+    py_modules=['rst2html5'],
     include_package_data=True,
     entry_points={
         'console_scripts': [
